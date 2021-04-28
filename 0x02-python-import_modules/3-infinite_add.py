4dba2a2da8a1026909cd8519dc4d7de5e0091c37
Mateo Londoño <mateolondono.u@gmail.com>
2021-04-28 15:21:33 -0500

    output correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,6 @@ import sys
 if __name__ == "__main__":
     acum = 0
     argc = len(sys.argv)
-    print(type(argc))
     for i in range(argc):
         if i > 0:
             add = int(sys.argv[i])
