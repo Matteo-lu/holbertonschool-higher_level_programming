0c48bb0e3db79cc2f12a78a99bbecabcfe9ac0b2
Mateo Londoño <mateolondono.u@gmail.com>
2021-04-27 21:40:18 -0500

    Documentation correction

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    """prints a string in uppercase followed by a new line"""
+    """prints a string in uppercase followed by a new line """
     if number < 0:
         last_digit = number % -10
         print("{}".format(-last_digit), end="")
