7076f9782b661188ea6ccad143278a0764c6a3b7
Mateo Londoño <mateolondono.u@gmail.com>
2021-04-27 21:14:15 -0500

    format correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,7 @@
 def uppercase(str):
     """prints a string in uppercase followed by a new line"""
     for x in str[:]:
-        character = ord(x)
-        if(character > 96 and character < 123):
-            character = character - 32
-        print("{}".format(chr(character)), end="")
+        if(ord(x) > 96 and ord(x) < 123):
+            x = chr(ord(x) - 32)
+        print("{}".format(x), end="")
     print("\n", end="")
