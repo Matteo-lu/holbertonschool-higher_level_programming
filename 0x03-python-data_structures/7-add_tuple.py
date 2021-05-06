870519610b3d037010ce8d4d28c1a0a7ac84bf60
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-06 09:18:06 -0500

    Pep correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,7 +17,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif j < 2 and i >= 2:
         new_tuple = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0)
     elif j < 2 and i < 2:
-        new_tuple = (tuple_a[0] + tuple_b[0]) , 0
+        new_tuple = (tuple_a[0] + tuple_b[0]), 0
     else:
         new_tuple = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
     return new_tuple
