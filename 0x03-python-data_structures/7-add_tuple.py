bf7d0324035772ea5abd5fa912488fef0d265db9
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-06 09:15:35 -0500

    Output correcton 1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -16,6 +16,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new_tuple = (tuple_a[0] + tuple_b[0]), (0 + tuple_b[1])
     elif j < 2 and i >= 2:
         new_tuple = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0)
+    elif j < 2 and i < 2:
+        new_tuple = (tuple_a[0] + tuple_b[0]) , 0
     else:
         new_tuple = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
     return new_tuple
