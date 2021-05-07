45215b47af72840dd617dd85a6ee4b6970cce06a
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-06 21:36:56 -0500

    3 Line correction

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = [[] for i in range(len(matrix))]
-    for i in range(len(matrix)):
-        new_matrix[i] = list((map(lambda x: x * x, matrix[i])))
-    return new_matrix
+    new = [list((map(lambda x: x*x, matrix[i])))for i in range(len(matrix))]
+    return new
