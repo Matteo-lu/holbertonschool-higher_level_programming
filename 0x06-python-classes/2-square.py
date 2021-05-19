07fefad5d6d2ca6834ea3224435a9ddb1096784a
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-18 21:40:31 -0500

    Uploading task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class that defines a square """
+
+
+class Square:
+    """Square class with corresponding methods"""
+
+    def __init__(self, size="0"):
+        """Initializes the data."""
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
