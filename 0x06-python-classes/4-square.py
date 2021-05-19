e0bb6d5751b5d2644699d3b198f2df074bfd5fac
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-18 22:44:58 -0500

    Uploading task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" class that defines a square """
+
+
+class Square:
+    """Square class with corresponding methods"""
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, new):
+        self.__size = new
