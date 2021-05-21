159d8da76d9b688bd663ebe7cab7d96b52747cfb
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-20 20:08:58 -0500

    Output error height correction

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Square class with corresponding methods"""
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.width = width
+        self.height = height
 
     @property
     def height(self):
@@ -17,9 +17,9 @@ class Rectangle:
     def height(self, value):
         self.__height = value
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
 
     @property
     def width(self):
