587bc85847694c6c07d9830a22fe1213e11723ba
Mateo Londoño <mateolondono.u@gmail.com>
2021-05-26 14:00:25 -0500

    max ath the begining

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,24 +9,18 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxIntegerMethods(unittest.TestCase):
     """Exceptions are documented in the same way as classes.
-
     The __init__ method may be documented in either the class level
     docstring, or as a docstring on the __init__ method itself.
-
     Either form is acceptable, but the two should not be mixed. Choose one
     convention to document the __init__ method and be consistent with it.
-
     Note:
         Do not include the `self` parameter in the ``Args`` section.
-
     Args:
         msg (str): Human readable string describing the exception.
         code (:obj:`int`, optional): Error code.
-
     Attributes:
         msg (str): Human readable string describing the exception.
         code (int): Exception error code.
-
     """
     def test_max(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
@@ -38,12 +32,7 @@ class TestMaxIntegerMethods(unittest.TestCase):
         self.assertEqual(max_integer([0]), 0)
         self.assertEqual(max_integer([-756]), -756)
         self.assertEqual(max_integer([1.1, 2.2, 4.4, 6.6]), 6.6)
-
-    def test_string_type(self):
-        self.assertRaises(TypeError, max_integer("h"))
-
-    def tes_string(self):
-        self.assertRaises(TypeError, max_integer([1, "h", 3, 4]))
+        self.assertEqual(max_integer([8, 4, 5, 7]), 8)
 
 if __name__ == '__main__':
     unittest.main()
