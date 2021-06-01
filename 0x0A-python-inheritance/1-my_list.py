5a4d962eac276e7db26a6126052f37ff5e6a9917
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-01 14:49:44 -0500

    Uploading task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" class that add print functionality to built-in list class.
+
+
+This Module is for the project 0x0A. Python - Inheritance
+proposed by Holberton school as a test for the design of
+MyList class that add print functionality to built-in list class.
+"""
+
+
+class MyList(list):
+    """MyList class to add functionality to built-in list
+
+    Args:
+        None
+
+    Attributes:
+        None
+
+    """
+
+    def print_sorted(self):
+        """print sorted list method"""
+
+        list_s = []
+        for element in self:
+            list_s.append(element)
+        list_s.sort()
+        print(list_s)
