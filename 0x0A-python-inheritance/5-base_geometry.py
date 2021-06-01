726c9c1ca5f0c0fffa7c0f75b2ae37283e273edf
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-01 15:45:17 -0500

    Uploading task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Empty BaseGeometry class
+
+
+This Module is for the project 0x0A. Python - Inheritance
+proposed by Holberton school as a test for the design of
+BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry empty class
+
+    Args:
+        None
+
+    Attributes:
+        None
+
+    """
+
+    pass
