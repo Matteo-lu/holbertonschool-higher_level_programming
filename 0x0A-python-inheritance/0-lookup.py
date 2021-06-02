8962022180f316dcd94c832fdbee9ad93266f7e2
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-01 21:19:58 -0500

    Documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+""" Module that returns the list of available attributes
+and methods of an object.
+
+
+This Module is for the project 0x0A. Python - Inheritance
+proposed by Holberton school as a test for the design of
+method that returns the list of available attributes and
+methods of an object.
+"""
+
+
 def lookup(obj):
+    """MyList class to add functionality to built-in list"""
     meth_at_obj = []
     return dir(meth_at_obj)
