7f493b02a331f3fb5cb8018b9a60d0aa993424df
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-02 14:59:12 -0500

    Uploading task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that reads a text file (UTF8) and prints it to stdout:
+"""
+
+
+def read_file(filename=""):
+    """
+    Function that reads a text file (UTF8) and prints it to stdout:
+    """
+
+    with open(filename,  encoding='utf-8') as my_file:
+        print(my_file.read(), end="")
