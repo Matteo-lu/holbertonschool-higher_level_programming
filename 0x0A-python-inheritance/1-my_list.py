f1f12720d5fee7305fad01a31aff5f31a0024185
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-01 22:36:55 -0500

    Documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""" class that add print functionality to built-in list class.
+"""
+class that add print functionality to built-in list class.
 
 
-This Module is for the project 0x0A. Python - Inheritance
-proposed by Holberton school as a test for the design of
-MyList class that add print functionality to built-in list class.
 """
 
 
