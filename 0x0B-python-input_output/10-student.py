3bcd77bf6eeda5194d1f746df0f1bc2a60658600
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-02 22:25:41 -0500

    Uploading task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+
+class Student:
+    """ My class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if type(attrs) is list:
+            for element in attrs:
+                if type(element) is str:
+                    new_dic = {}
+                    dic = self.__dict__
+                    for element in attrs:
+                        for key, value in dic.items():
+                            if key == element:
+                                new_dic[key] = value
+            return new_dic
+        else:
+            return self.__dict__
