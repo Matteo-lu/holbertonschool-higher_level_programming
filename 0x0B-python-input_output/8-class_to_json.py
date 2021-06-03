ea3877e9ae84b34f16ec085f78aa25aa6e2ce889
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-02 21:36:22 -0500

    /usr/bin/python3 /home/matteo_lu/holbertonschool-higher_level_programming/0x0B-python-input_output/8-main_2.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that  returns the dictionary description
+"""
+
+
+def class_to_json(obj):
+    """
+    Function that  returns the dictionary description
+    """
+    return (obj.__dict__)
