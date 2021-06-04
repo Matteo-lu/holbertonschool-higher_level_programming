323370060a493b09a96c629f171a2d802f566fdf
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-03 21:42:01 -0500

    Uploading task

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module that returns a list of lists of integers representing
+the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+
+    my_list = []
+    if n <= 0:
+        return (my_list)
+    my_list = [[1],[1,1]]
+    for i in range(1, (n - 1)):
+        count = [1]
+        for j in range(0, len(my_list[i]) - 1):
+            count.extend([my_list[i][j] + my_list[i][j+1]])
+        count += [1]
+        my_list.append(count)
+    return (my_list)
