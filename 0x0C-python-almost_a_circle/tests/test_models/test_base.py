1f23353bc8fce99eb916828d493e96811e583fab
Mateo Londoño <mateolondono.u@gmail.com>
2021-06-08 17:45:37 -0500

    Uploading test directory

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,29 +4,13 @@
 
 
 import unittest
+import models.base
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBaseClass(unittest.TestCase):
     """Test class to test Base class"""
-
-    @classmethod
-    def setUpClass(self):
-        """
-        setUp class to import modules
-
-        Args:
-            None
-        """
-
-    def setUp(self):
-        """
-        setUp method to import modules
-
-        Args:
-            None
-        """
-        pass
 
     def test_Module_documentation(self):
         """
@@ -35,7 +19,6 @@ class TestBaseClass(unittest.TestCase):
         Args:
             None
         """
-        import models.base
         mod_doc = (models.base.__doc__)
         self.assertTrue(len(mod_doc.strip().splitlines()) >= 4)
 
@@ -93,6 +76,41 @@ class TestBaseClass(unittest.TestCase):
         self.assertEqual(b5.id, 4)
         self.assertEqual(b6.id, -9)
         self.assertEqual(b7.id, 0)
+
+    def test_json_string(self):
+        """
+        test json string
+
+        Args:
+            None
+        """
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        self.assertEqual(type(Base.to_json_string([dictionary])), str)
+
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    """def test_save_to_file(self):
+        test instanses of base class
+
+        Args:
+            None
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            ret = file.read()
+        self.assertEqual(ret, second)"""
+
+    def test_from_json_string(json_string):
+        """
+        test json string
+
+        Args:
+            None
+        """
 
 if __name__ == '__main__':
     unittest.main()
