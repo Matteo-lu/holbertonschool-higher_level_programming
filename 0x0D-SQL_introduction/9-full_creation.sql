2597312c1a1f9145bc9fea49735e61b4f79a0480
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-12 17:55:26 -0500

    Uplading task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), score INT);
 INSERT INTO first_table (`id`,`name`,`score`) VALUES (1, "John", 10);
 INSERT INTO first_table (`id`,`name`,`score`) VALUES (2, "John", 3);
 INSERT INTO first_table (`id`,`name`,`score`) VALUES (3, "John", 14);
