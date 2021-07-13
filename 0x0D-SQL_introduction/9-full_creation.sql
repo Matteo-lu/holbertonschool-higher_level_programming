8a3cc3e4cc23f3d6b62a4c0633c8e84e07567ad0
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-12 19:30:57 -0500

    Uploading task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 CREATE TABLE IF NOT EXISTS second_table like first_table;
 ALTER TABLE second_table ADD score INT;
-INSERT INTO IF EXISTS second_table (`id`,`name`,`score`)
+INSERT INTO second_table (`id`,`name`,`score`)
 VALUES
 (1, "John", 10),
 (2, "Alex", 3),
