983e6289f1cb8eea3d9ea84add41a0bb1f4ccb99
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-13 19:52:31 -0500

    Uploading

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(id INTEGER UNIQUE AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES states(state_id))
-    type=InnoDB;
+    name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id))
+    ENGINE=InnoDB;
