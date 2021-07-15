bbee753fba76dfa762cab9443eabb78baeaf5b1e
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-14 19:25:00 -0500

    Uploading task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,8 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.title
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY title ASC;
+ORDER BY title ASC, name ASC;
