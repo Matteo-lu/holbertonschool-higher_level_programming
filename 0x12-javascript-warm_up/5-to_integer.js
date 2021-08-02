e3a927d97f3b2eb04750669a5412ac812fc85e60
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-02 17:43:52 -0500

    Uploading task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
-const prhase = 'My number: ';
+const prhase = 'My number:';
 const value = parseInt(process.argv[2]);
 
 if (Number.isNaN(value)) {
