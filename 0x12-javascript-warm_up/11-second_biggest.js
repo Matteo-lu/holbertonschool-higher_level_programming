02d0ec20d84d134f62707634ef5cc208423e3160
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-02 19:23:23 -0500

    Fixing style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
 const process = require('process');
-const arguments = process.argv;
-let myArray = [];
+const myArray = [];
 let max;
 let i;
 let j = 2;
 
-if (arguments.length < 3) {
-  console.log(0)
-} else if (arguments.length < 4) {
-  console.log(0)
+if (process.argv.length < 3) {
+  console.log(0);
+} else if (process.argv.length < 4) {
+  console.log(0);
 } else {
-  for (i = 0; i < (arguments.length - 2); i++) {
-    myArray[i] = parseInt(arguments[j]);
+  for (i = 0; i < (process.argv.length - 2); i++) {
+    myArray[i] = parseInt(process.argv[j]);
     j++;
   }
   max = Math.max.apply(null, myArray);
   myArray.splice(myArray.indexOf(max), 1);
   max = Math.max.apply(null, myArray);
-  console.log(max)
+  console.log(max);
 }
