26d6a1077c21915f83cf583119c6ecdefb4188d1
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-03 20:54:59 -0500

    Uploading task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    let character = c;
+    if (character === undefined) {
+      character = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(character.repeat(this.size));
+    }
+  }
+}
+module.exports = Square;
