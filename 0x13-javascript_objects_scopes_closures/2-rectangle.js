008537419f420e1cdcc432218fc7ab07ffd24edf
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-03 19:57:53 -0500

    Uploading task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !w || !h) {
-      const objt = {};
-    } else {
+    if (w > 0 && h > 0 && w && h) {
       this.width = w;
       this.height = h;
     }
