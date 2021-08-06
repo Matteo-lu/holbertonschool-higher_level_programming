e1304174ea649513c45290e863f534f552a70388
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-06 15:34:21 -0500

    Uploading task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" script that lists all cities from the database hbtn_0e_4_usa.
+
+This Module is for the project 0x0F. Python - Object-relational
+mapping proposed by Holberton school as a test for the implementation
+of MySQLdb module with hbtn_0e_0_usa database.
+"""
+
+if __name__ == "__main__":
+   import sys, MySQLdb
+
+   args_list = sys.argv
+   username = args_list[1]
+   password = args_list[2]
+   database_name = args_list[3]
+
+   db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+
+   cursor = db.cursor()
+
+   cursor.execute("SELECT VERSION()")
+
+   sql = "SELECT cities.id, cities.name, states.name \
+         FROM cities LEFT JOIN states \
+         ON cities.state_id = states.id \
+         ORDER BY cities.id"
+
+   try:
+      # Execute the SQL command
+      cursor.execute(sql)
+      # Fetch all the rows in a list of lists.
+      results = cursor.fetchall()
+      for row in results:
+         print(row)
+   except:
+      print ("Error: unable to fecth data")
+
+   db.close()
