df9cf9a838ed7534fbd92a5ab10e8ccc2b429d8d
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-06 15:33:47 -0500

    importing correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,34 +7,32 @@ mapping proposed by Holberton school as a test for the implementation
 of MySQLdb module with hbtn_0e_0_usa database.
 """
 
-import sys, MySQLdb
-
-args_list = sys.argv
-username = args_list[1]
-password = args_list[2]
-database_name = args_list[3]
-
-db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
-
-cursor = db.cursor()
-
-cursor.execute("SELECT VERSION()")
-
-sql = "SELECT * FROM states \
-		WHERE name like 'N%' \
-		ORDER BY id ASC"
-
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Fetch all the rows in a list of lists.
-   results = cursor.fetchall()
-   for row in results:
-      print(row)
-except:
-   print ("Error: unable to fecth data")
-
-db.close()
-
 if __name__ == "__main__":
-    main()
+	import sys, MySQLdb
+
+	args_list = sys.argv
+	username = args_list[1]
+	password = args_list[2]
+	database_name = args_list[3]
+
+	db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+
+	cursor = db.cursor()
+
+	cursor.execute("SELECT VERSION()")
+
+	sql = "SELECT * FROM states \
+			WHERE name like 'N%' \
+			ORDER BY id ASC"
+
+	try:
+	# Execute the SQL command
+		cursor.execute(sql)
+		# Fetch all the rows in a list of lists.
+		results = cursor.fetchall()
+		for row in results:
+			print(row)
+	except:
+		print ("Error: unable to fecth data")
+
+	db.close()
