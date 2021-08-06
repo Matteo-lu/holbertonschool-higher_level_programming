40515ad37eaa03da937a880022b9922f9691e65f
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-06 15:40:02 -0500

    sorted correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
    sql = "SELECT cities.id, cities.name, states.name \
          FROM cities LEFT JOIN states \
          ON cities.state_id = states.id \
-         ORDER BY cities.id"
+         ORDER BY ASC cities.id"
 
    try:
       # Execute the SQL command
