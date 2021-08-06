9f038d05040be745c549c72655f0efeda876d0bf
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-06 11:07:58 -0500

    Uploading task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa
+
+This Module is for the project 0x0F. Python - Object-relational
+mapping proposed by Holberton school as a test for the implementation
+of MySQLdb module with hbtn_0e_0_usa database.
+"""
+
+import sys, MySQLdb
+
+args_list = sys.argv
+username = args_list[1]
+password = args_list[2]
+database_name = args_list[3]
+
+db = MySQLdb.connect("localhost", username, password, database_name)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT VERSION()")
+
+sql = "SELECT * FROM states"
+
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      print(row)
+except:
+   print ("Error: unable to fecth data")
+
+db.close()
