2dc3df2e73ab6ca21a32b241dc618f48310eeb1d
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-07 12:59:18 -0500

    execution correction

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -26,26 +26,22 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT VERSION()")
+    cursor.execute("""SELECT cities.name \
+                    FROM cities LEFT JOIN states \
+                    ON cities.state_id = states.id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id""", (state_name,))
 
-    try:
-        cursor.execute("""SELECT cities.name \
-                        FROM cities LEFT JOIN states \
-                        ON cities.state_id = states.id \
-                        WHERE states.name = %s \
-                        ORDER BY cities.id""", (state_name,))
-        results = cursor.fetchall()
-        count = 0
-        if len(results) > 0:
-            for row in results:
-                count += 1
-                if count < (len(results)):
-                    print('{}, '.format(row[0]), end="")
-                else:
-                    print('{}'.format(row[0]))
-        else:
-            print()
-    except:
-        print("Error: unable to fecth data")
+    results = cursor.fetchall()
+    count = 0
+    if len(results) > 0:
+        for row in results:
+            count += 1
+            if count < (len(results)):
+                print('{}, '.format(row[0]), end="")
+            else:
+                print('{}'.format(row[0]))
+    else:
+        print()
 
     db.close()
