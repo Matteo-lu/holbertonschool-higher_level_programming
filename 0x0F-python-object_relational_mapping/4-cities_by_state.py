ce3f8246e9c0fae4ed3d2ba121d2eccb957a10c8
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-07 12:56:37 -0500

    execution correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -25,19 +25,13 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT VERSION()")
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                    FROM cities LEFT JOIN states \
+                    ON cities.state_id = states.id \
+                    ORDER BY cities.id ASC")
 
-    sql = "SELECT cities.id, cities.name, states.name \
-            FROM cities LEFT JOIN states \
-            ON cities.state_id = states.id \
-            ORDER BY cities.id"
-
-    try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except:
-        print("Error: unable to fecth data")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
 
     db.close()
