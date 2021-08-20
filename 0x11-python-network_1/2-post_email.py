be40ba0074082d536b11d4926bcb8e10888bf1f5
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-20 14:18:22 -0500

    Style correction

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that takes in a URL and an email, sends a POST requestto the passed
-URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)"""
 
 
 import urllib.request
