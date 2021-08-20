26f748365233a3b0311a4341029c5791db61e2be
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-20 14:51:28 -0500

    import statment correction

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,19 +4,20 @@ URL with the email as a parameter, and displays the body of the
 response (decoded in utf-8)"""
 
 
-import urllib.parse
-import urllib.request
-import sys
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-values = {'email': email}
+    values = {'email': email}
 
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(req) as response:
-    html = response.read().decode('utf-8')
-    print(html)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('utf-8')
+        print(html)
