ea5eb73cb962067361f92c91edc583b78bd48aa5
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-20 11:14:42 -0500

    Uploading task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response."""
+
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+
+my_dict = response.headers.__dict__
+for element in my_dict['_headers']:
+    if element[0] == 'X-Request-Id':
+        print(element[1])
