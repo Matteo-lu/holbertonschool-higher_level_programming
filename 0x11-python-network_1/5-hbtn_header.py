c695abec9a6504ca881cf34776a52b7dee25d6b0
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-20 20:59:14 -0500

    loop correction

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    for key in r.headers:
+    print(r.headers.keys():)
+    for key in r.headers.keys():
         if key == 'X-Request-Id':
             print(r.headers[key])
