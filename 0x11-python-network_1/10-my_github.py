0e4ad66c31f3b54d598565e79980e7464c7b0233
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-20 20:37:41 -0500

    Uploading task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    token = sys.argv[2]
+    username = sys.argv[1]
+    headers = {'Authorization': 'token ' + token}
+    login = requests.get('https://api.github.com/user', headers=headers)
+
+    print(login.json()['id'])
