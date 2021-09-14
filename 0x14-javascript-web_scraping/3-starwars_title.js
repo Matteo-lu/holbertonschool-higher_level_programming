f00ad3051edfcd1e5ac83052ebe24418f4509114
Matteo-lu <mateolondono.u@gmail.com>
2021-09-14 15:37:29 -0500

    Uploading task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const requestURL = 'https://swapi-api.hbtn.io/api/films/';
+const movieId = process.argv[2];
+const request = require('request');
+let obj;
+
+request(requestURL + movieId, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    obj = JSON.parse(body);
+    console.log(obj.title);
+  }
+});
