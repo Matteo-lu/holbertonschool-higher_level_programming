23c5b17d161f3bc905e683b02fba7ad1e5b09918
Matteo-lu <mateolondono.u@gmail.com>
2021-09-14 20:25:13 -0500

    Uploading task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const requestURL = process.argv[2];
+const request = require('request');
+let obj;
+let countTask = 0;
+let i;
+let j;
+const newJSON = {};
+
+request(requestURL, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    obj = JSON.parse(body);
+    for (j = 1; j <= 10; j++) {
+      for (i = 0; i < obj.length; i++) {
+        if (obj[i].userId === j && obj[i].completed === true) {
+          countTask += 1;
+        }
+      }
+      newJSON[j.toString()] = countTask;
+      countTask = 0;
+    }
+    console.log(newJSON);
+  }
+});
